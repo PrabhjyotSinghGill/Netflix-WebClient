f8d9aa26ca51ee56ed29e92b6dce7d91ddff5f96
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-01-11 13:19:46 -0500

    5) TMDB API :TMDB API implemented using Axios Library.

---
+++ src/Requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "45c5cf451256fdee4cb4ae5185bfd9e9";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
+// Working API link
+//https://api.themoviedb.org/3/trending/all/week?api_key=45c5cf451256fdee4cb4ae5185bfd9e9&language=en-US
