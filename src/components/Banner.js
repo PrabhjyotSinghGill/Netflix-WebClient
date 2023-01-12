dcdd141f9e08d95023cf4425edca857136e34027
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-01-12 03:42:45 -0500

    6) ROW Component : isLargeRow props are added.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,7 +18,7 @@ function Banner() {
     }
     fetchData();
   }, []);
-  
+
   console.log(movie);
 
   const truncate = (string, n) => {
@@ -29,22 +29,21 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundColor: "black",
-        // backgroundImage: `url("/Netflix.png")`,
+        // backgroundColor: "black",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.This is a test description.`,
-            150
-          )}
+          {truncate(movie?.overview, 500)}
         </h1>
       </div>
       <div className="banner--fadeBottom"></div>
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../views/HomeScreen.css";
 import NavBar from "../components/NavBar";
 import Banner from "../components/Banner";
+import Row from "../components/Row.js";
+import requests from "../Requests";
 
 function HomeScreen() {
   return (
@@ -11,6 +13,18 @@ function HomeScreen() {
       {/* Banner*/}
       <Banner></Banner>
       {/* Rows */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      ></Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
     </div>
   );
 }
